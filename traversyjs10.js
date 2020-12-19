ea64c319e7fdcd997044bef8349b8bce443ecd97
spaceC00kie <kirstie317@gmail.com>
2020-12-18 22:16:26 -0500

    print array item and JSON.stringify

---
+++ traversyjs10.js
@@ -0,0 +1,28 @@
+  //array of objects
+  
+  const todos = [
+    {
+      id: 1,
+      text: "Take out trash",
+      isCompleted: true,
+    },
+    {
+      id: 2,
+      text: "Do laundry",
+      isCompleted: true,
+    },
+    {
+      id: 3,
+      text: "Load dishwasher",
+      isCompleted: false,
+    },
+  ]
+  
+  //print an item from the array
+  //console.log(todos[1].text)
+  
+  //how to send data to a server
+  //const todoJSON = JSON.stringify(todos)
+  //console.log(todoJSON)
+
+  
